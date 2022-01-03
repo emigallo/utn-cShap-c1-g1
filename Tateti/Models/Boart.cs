d4b39cc272b2f47174ec89909e215c11f468ff77
Facundo Daniel Aparicio <faaparic@emeal.nttdata.com>
2022-01-03 16:00:48 -0300

    primer commit

---
+++ Tateti/Models/Boart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tateti.Models
+{
+    public class Boart
+    {
+        //matriz de 9 posiciones para el tablero
+    }
+}
---
+++ Tateti/Models/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tateti.Models
+{
+    public class File
+    {
+        public string type { get; set; }
+    }
+}
