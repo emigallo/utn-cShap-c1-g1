8d9b6e8d582f7f0caf1b96f196d01d51d9f563dd
tomeameri <tomasezequiel.ameri@everis.nttdata.com>
2022-01-06 16:30:26 -0300

    for

--- Tateti/Models/Boart.cs
+++ Tateti/Models/Boart.cs
@@ -87,42 +87,7 @@ namespace Tateti.Models
             // 0,2 1,2 2,2
         }
 
-        //public File WinnerByRow()
-        //{
-        //    //usar un solo for
-        //    for (int i = 0; i < _position.GetLength(0); i++)
-        //    {
-        //        for (int j = 0; j < 1; j++)
-        //        {
-        //            if (_position[i, j] != null && _position[i, j + 1] != null && _position[i, j + 2] != null)
-        //                if ((_position[i, j].Type == _position[i, (j + 1)].Type) && _position[i, (j + 2)].Type == _position[i, j].Type)
-        //                    return _position[i, j];
-        //        }
-        //    }
-        //    return null;
-        //    // 0,0 0,1 0,2
-        //    // 1,0 1,1 1,2
-        //    // 2,0 2,1 2,2
-        //}
-
-        //public File WinnerByCol()
-        //{
-        //    //usar un solo for
-
-        //    for (int i = 0; i < 1; i++)
-        //    {
-        //        for (int j = 0; j < _position.GetLength(1); j++)
-        //        {
-        //            if (_position[i, j] != null && _position[i + 1, j] != null && _position[i + 2, j] != null)
-        //                if ((_position[i, j].Type == _position[(i + 1), j].Type) && _position[(i + 2), j].Type == _position[i, j].Type)
-        //                    return _position[i, j];
-        //        }
-        //    }
-        //    return null;
-        //    // 0,0 1,0 2,0
-        //    // 0,1 1,1 1,2
-        //    // 0,2 1,2 2,2
-        //}
+       
 
         public File WinnerByDig()
         {
@@ -146,44 +111,7 @@ namespace Tateti.Models
         }
 
 
-        // public File Winner()
-        //{
-        //if (((_position[0].Type == _position[1].Type) && _position[2].Type == _position[1].Type) ||
-        //    ((_position[3].Type == _position[4].Type) && _position[5].Type == _position[3].Type) ||
-        //    ((_position[6].Type == _position[7].Type) && _position[8].Type == _position[7].Type) ||
-        //    ((_position[0].Type == _position[3].Type) && _position[6].Type == _position[0].Type) ||
-        //    ((_position[1].Type == _position[4].Type) && _position[7].Type == _position[1].Type) ||
-        //    ((_position[2].Type == _position[5].Type) && _position[8].Type == _position[2].Type) ||
-        //    ((_position[0].Type == _position[4].Type) && _position[8].Type == _position[0].Type) ||
-        //    ((_position[2].Type == _position[4].Type) && _position[6].Type == _position[0].Type))
-        //    return true;
-
-        //metodos individuales 
-
-
-        //if ((_position[0].Type == _position[1].Type) && _position[2].Type == _position[1].Type) 
-        //    return _position[0];
-        //if ((_position[3].Type == _position[4].Type) && _position[5].Type == _position[3].Type)
-        //    return _position[3];
-        //if ((_position[6].Type == _position[7].Type) && _position[8].Type == _position[7].Type)
-        //    return _position[6];
-        //if ((_position[0].Type == _position[3].Type) && _position[6].Type == _position[0].Type)
-        //    return _position[0];
-        //if ((_position[1].Type == _position[4].Type) && _position[7].Type == _position[1].Type)
-        //    return _position[1];
-        //if ((_position[2].Type == _position[5].Type) && _position[8].Type == _position[2].Type)
-        //    return _position[2];
-        //if ((_position[0].Type == _position[4].Type) && _position[8].Type == _position[0].Type)
-        //    return _position[0];
-        //if ((_position[2].Type == _position[4].Type) && _position[6].Type == _position[0].Type)
-        //    return _position[2];
-        //else
-        //    return null;
-
-        //si hay ganador se anuncia
-        //si es null sigue
-
-        //}
+        
 
 
     }
